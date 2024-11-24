775328556e05b2cba2eafb3636ba05ba4a1b02e4
Adam T <aovert.at@gmail.com>
2024-11-24 19:57:30 +0000

    added page functionality to upcoming movies page using pagination

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
+import Pagination from "@mui/material/Pagination";
 
 
 const UpcomingMoviesPage = (props) => {
 
-  const {  data, error, isLoading, isError }  = useQuery('upcomingMovies', getUpcomingMovies)
+const [currentPage, setCurrentPage] = useState(1)
+
+  const {  data, error, isLoading, isError }  = useQuery(
+    ['upcomingMovies',currentPage],
+    () => getUpcomingMovies(currentPage),
+    { keepPreviousData: true}
+  )
+  
 
   if (isLoading) {
     return <Spinner />
@@ -24,14 +32,30 @@ const UpcomingMoviesPage = (props) => {
   localStorage.setItem('playlist', JSON.stringify(playlist))
   const addToPlaylist = (movieId) => true 
 
+  const handlePageChange = (event, page) =>
+    setCurrentPage(page);
+
   return (
-    <PageTemplate
-        title="Discover Upcoming Movies"
+    <div>
+      <PageTemplate
+        title="Upcoming Movies"
         movies={movies}
-        action={(movie) => {
-          return <AddToPlaylistIcon movie={movie} />
+        action={(movie) => <AddToPlaylistIcon movie={movie} />}
+      />
+
+      <Pagination
+        count={data.total_pages} 
+        page={currentPage} 
+        onChange={handlePageChange} 
+        color="primary"
+        sx={{
+          mt: 4, 
+          display: "flex",
+          justifyContent: "center",
         }}
-    />
+      />
+    </div>
   );
 };
+
 export default UpcomingMoviesPage;
